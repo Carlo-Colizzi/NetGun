ebc0e702d982d5ae1ad2a4cf773a200c8d5bf74d
ducky <carlo.colizzi@gmail.com>
2022-12-30 12:57:24 +0100

    + Added "Source_Code" Package and "Business_Logic" Package + Added Class "Target" with checkIp() and checkPorts() methods, now the scan target can be checked

---
+++ Source_Code/Business_Logic/Target.py
@@ -0,0 +1,25 @@
+import re
+
+class Target:
+
+    def __init__(self, ip: str = "127.0.0.1", ports_range: str = "0-65535"):
+        assert self.__class__.check_ip(ip) == True, "Invalid IPv4 Address, check_ip(ip) failed"
+        assert self.__class__.check_ports(ports_range) == True, "Invalid Ports Range, check_ports(ports_range) failed"
+        self.ip = ip
+        self.ports_range = ports_range
+
+    @classmethod
+    def check_ip(cls, ip: str):
+        pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+        if re.match(pattern, ip):
+            return True
+        return False
+
+    @classmethod
+    def check_ports(cls, ports:str):
+        pattern = "^([0-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-5][0-9][0-9][0-9]|6[0-4][0-9][0-9]|65[0-4][0-9]|655[0-2])-(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]|6[0-4][0-9][0-9]|[1-5][0-9][0-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9]|[0-9])$";
+        if re.match(pattern, ports):
+            values = ports.split("-", 1)
+            if values[0] <= values[1]:
+                return True
+        return False
