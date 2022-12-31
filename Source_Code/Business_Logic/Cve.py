2cc3d351866c2f75d927b9b34359e002d9f49e56
Giu57 <76096146+Giu57@users.noreply.github.com>
2022-12-31 16:17:22 +0100

    + Changed Cve class

--- Source_Code/Business_Logic/Cve.py
+++ Source_Code/Business_Logic/Cve.py
@@ -1,27 +1,43 @@
-import os, json
+import requests
+from bs4 import BeautifulSoup
 
 
 class Cve:
-	def __init__(self, service):
-		self.service = service
+    # url to connect to exploitDB
+    url = "https://www.exploit-db.com/search"
 
-	
-	# search CVEs with searchsploit and return a CVEs dictionary 
-	def search_Cve(self) -> dict:
-		os.system(f"searchsploit -j {self.service} >> $HOME/Documents/result_search_cve.json")
-		file = open(f"$HOME/Documents/result_search_cve.json")
-		dictionary_cve = json.loads(file.read())
-		return dictionary_cve
+    version: str
+    type: str
+    platform: str
+    port: int
+    result: dict
 
-	# control which CVE is valide
-	def are_valide(self, cve_dictionary):
-		pass
-			
+    # Variable created because direct request with default user agent of python is blocked by firewall
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " \
+                 "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+    def __init__(self, version, type=None, platform=None, port=None):
+        assert version is not None, "Version not exists"
+        self.version = version
+        self.type = type
+        self.platform = platform
+        self.port = None
+
+    # searches verified CVEs with the service
+    def search_cve(self) -> dict:
+        params = "?q=" + self.version + "&verified=true"
+        self.url += params
+
+        if self.platform is not None:
+            self.url += f"&platform={self.platform}"
+
+        response = requests.get(self.url, headers={"User-Agent": self.USER_AGENT})
+        data = response.content
+        print(data)
 
 
 
-service = input("Inserisci servizio:\n")
-cve = Cve(service)
-cve_dictionary = cve.search_Cve()
+version = input("Inserisci servizio:\n")
+cve = Cve(version)
+cve_dictionary = cve.search_cve()
 print(cve_dictionary)
-
