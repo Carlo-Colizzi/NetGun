d942d6136135e9f1c7bb8e0fab5901678d10bdf2
giu57 <giulio.incoronato01@gmail.com>
2023-01-02 13:54:21 -0500

    + added verified_cve and scrapping_site functions

--- Source_Code/Business_Logic/Cve.py
+++ Source_Code/Business_Logic/Cve.py
@@ -11,31 +11,34 @@ def get_elements_from_json(path: str) -> dict:
 def remove_file(path: str):
     os.system("rm " + path)
 
+def scrapping_site(url:str, search_tag:str) -> dict:
+    pass
 
 class Cve:
-    path = "../exploits_result.json"
+    path = "./exploits_result.json"
     version: str
     results: dict
 
     def __init__(self, version: str):
         assert version is not None, "Version not exists"
         self.version = version
+        os.system("searchsploit -u")
 
     def search_cve(self):
-        command_line = "searchsploit -u -j " + self.version + " >> " + self.path
+        command_line = "searchsploit -w -j " + self.version + " >> " + self.path
         os.system(command_line)
         self.results = get_elements_from_json(self.path)
         remove_file(self.path)
-        # print(self.results)
+        print(self.results)
         for cve in self.results['RESULTS_EXPLOIT']:
-            Cve.validate_cve(cve)
+            Cve.verified_cve(cve)
 
     @classmethod
-    def validate_cve(cls, cve):
+    def verified_cve(cls, cve):
         header = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " \
                                 "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
         response = requests.get(cve['URL'], headers=header)
-        print(response.url)
+        scrapping_site(response.url, "verified")
 
 
 version = "http"
