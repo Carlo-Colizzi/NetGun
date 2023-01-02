4b8641f0ec20f4a0208b2d4f7e48e7c2139c176e
kali-seph <kali-seph@seph-infinito.homenet.telecomitalia.it>
2023-01-02 12:45:38 +0100

    + added the option button, main label and color set

--- Source_Code/View/main_gui.py
+++ Source_Code/View/main_gui.py
@@ -8,12 +8,47 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        # configurazione window
+        # configuration window
         self.title("NetGun")
         self.geometry(f"800x600")
+
+        # variables
+        color_option_variable = customtkinter.StringVar(value="System")
         
-        # frame con titolo e pulsante opzioni
-        self.main_frame = customtkinter.CTkFrame(self, corner_radius=1)
+        def options_settings():
+            
+            window_options = customtkinter.CTkToplevel()
+            window_options.geometry(f"400x200")
+            window_options.title("Options")
+
+            def change_mode_appearence(new_mode):
+                customtkinter.set_appearance_mode(new_mode)
+
+            # frame options
+            frame_options = customtkinter.CTkFrame(window_options, corner_radius=5)
+            frame_options.grid(pady=30, padx=30, sticky="nsew")
+            frame_options.place(relx=0.5, rely=0.5, anchor="c")
+
+            # color mode set
+            color_mode_label = customtkinter.CTkLabel(master=frame_options, text="Color Mode:")
+            color_mode_label.grid(row=0, column=0, sticky="w")
+
+            appearence_mode = customtkinter.CTkOptionMenu(master=frame_options, values=["System", "Dark", "Light"], command=change_mode_appearence, variable=color_option_variable)
+            appearence_mode.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
+
+        # main frame with options and label
+        self.main_frame = customtkinter.CTkFrame(self, corner_radius=4)
+        self.main_frame.grid(pady=30, padx=30, sticky="nsew")
+        self.main_frame.place(relx=0.5, rely=0.5, anchor="c")
+
+        # label
+        self.main_frame_label = customtkinter.CTkLabel(self.main_frame, text="NetGun", font=customtkinter.CTkFont(size=18, weight="bold"))
+        self.main_frame_label.grid(row=0, column=0, sticky="w")
+
+        # options button
+        self.options_button = customtkinter.CTkButton(self.main_frame, text="Options", command=options_settings)
+        self.options_button.grid(row=0, column=1, sticky="e")
+
 
 if __name__ == "__main__":
     app = App()
