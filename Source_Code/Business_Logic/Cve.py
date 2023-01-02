f4a96d16600626952fdca14db4ac1ade29cf8964
giu57 <giulio.incoronato01@gmail.com>
2023-01-02 15:14:42 -0500

    + Modified verified_cve method and fix some issues

--- Source_Code/Business_Logic/Cve.py
+++ Source_Code/Business_Logic/Cve.py
@@ -7,13 +7,18 @@ def get_elements_from_json(path: str) -> dict:
         dict = json.load(file)
     return dict
 
-
 def remove_file(path: str):
     os.system("rm " + path)
 
-def scrapping_site(url:str, search_tag:str) -> dict:
-    pass
+def found_element_in_html(text_site:str, search_tag:str) -> bool:
+    soup = BeautifulSoup(text_site, "html.parser")
 
+    if soup.find_all(search_tag)is not None:
+        return True
+    
+    return False
+
+    
 class Cve:
     path = "./exploits_result.json"
     version: str
@@ -25,20 +30,21 @@ class Cve:
         os.system("searchsploit -u")
 
     def search_cve(self):
-        command_line = "searchsploit -w -j " + self.version + " >> " + self.path
+        command_line = "searchsploit -j " + self.version + " >> " + self.path
         os.system(command_line)
-        self.results = get_elements_from_json(self.path)
+        results = get_elements_from_json(self.path)
         remove_file(self.path)
-        print(self.results)
-        for cve in self.results['RESULTS_EXPLOIT']:
-            Cve.verified_cve(cve)
+        for cve in results['RESULTS_EXPLOIT']:
+            if cve["Codes"] is not None:
+                if Cve.verified_cve(cve) is True:
+                    print(cve)
+                    
 
     @classmethod
-    def verified_cve(cls, cve):
-        header = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " \
-                                "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-        response = requests.get(cve['URL'], headers=header)
-        scrapping_site(response.url, "verified")
+    def verified_cve(cls, cve:dict) -> bool:
+        if cve["Verified"] == "1":
+            return True
+        return False
 
 
 version = "http"
