f2230da3f34f98eb88e15688fbe6825a2a3e1742
Giu57 <76096146+Giu57@users.noreply.github.com>
2023-01-02 03:22:20 +0100

    + Changed Cve class and created new functions/methods

--- Source_Code/Business_Logic/Cve.py
+++ Source_Code/Business_Logic/Cve.py
@@ -1,67 +1,43 @@
-import requests, json
+import os, json, requests
 from bs4 import BeautifulSoup
 
 
-# function that replaces space with '%20' for create a valid URL
-def replace_space_with_url_encode(url: str) -> str:
-    return url.replace(" ", "%20")
+def get_elements_from_json(path: str) -> dict:
+    with open(path) as file:
+        dict = json.load(file)
+    return dict
 
 
-def html_to_json(response):
-    soup = BeautifulSoup(response.text, "html.parser")
-    elements = soup.select("table.exploit_list tbody tr")
-    for element in elements:
-        print(element)
+def remove_file(path: str):
+    os.system("rm " + path)
 
 
 class Cve:
-    # url to connect to exploitDB
-    url = "https://www.exploit-db.com/search"
-
+    path = "../exploits_result.json"
     version: str
-    platform: str
-    port: int
-    result: dict
+    results: dict
 
-    # Variable created because direct request with default user agent of python is blocked by firewall
-    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " \
-                 "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
-    def __init__(self, version, platform=None, port=None):
+    def __init__(self, version: str):
         assert version is not None, "Version not exists"
         self.version = version
-        self.platform = platform
-        self.port = port
 
-    # searches verified CVEs with the service (and platform and port if exists)
-    def search_cve(self) -> dict:
-        params = f"?q=" + self.version
-        self.url += params
-        self.url = Cve.filter_searching_string(self.url, self.port, self.platform)
-        self.url += "&verified=true"
-        self.url = replace_space_with_url_encode(self.url)
-        response = requests.get(self.url.lower(), headers={"User-Agent": self.USER_AGENT})
-        print(self.url.lower())
-        html_to_json(response)
+    def search_cve(self):
+        command_line = "searchsploit -u -j " + self.version + " >> " + self.path
+        os.system(command_line)
+        self.results = get_elements_from_json(self.path)
+        remove_file(self.path)
+        # print(self.results)
+        for cve in self.results['RESULTS_EXPLOIT']:
+            Cve.validate_cve(cve)
 
-        if response.status_code != 200:
-            raise Exception(f"Error! Cannot reach ExploitDB server")
-
-    # add filters to searching string
     @classmethod
-    def filter_searching_string(cls, url, port=None, platform=None) -> str:
-        if platform is not None:  # control if platform is not empty from scan result
-            url += f"&platform={platform}"
-
-        if port is not None:  # control if port number is not empty from scan result
-            url += f"&port={port}"
-
-        return url
+    def validate_cve(cls, cve):
+        header = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " \
+                                "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+        response = requests.get(cve['URL'], headers=header)
+        print(response.url)
 
 
-version = "http apache"  # input("Inserisci servizio:\n")
-platform = "Ruby"  # input("Inserisci Piattaforma:\n")
-port = 80  # input("Inserisci porta:\n")
-cve = Cve(version, platform, port)
-cve_dictionary = cve.search_cve()
-print(cve_dictionary)
+version = "http"
+cve = Cve(version)
+cve.search_cve()
