9a907ff0d0f87abdff6ed7d4fab4553d5fdf61a1
Giu57 <76096146+Giu57@users.noreply.github.com>
2023-01-04 01:06:31 +0100

    + added comments and reduced code

--- Source_Code/Business_Logic/cve.py
+++ Source_Code/Business_Logic/cve.py
@@ -1,35 +1,40 @@
-import  requests, json, time
+import  requests, time
 from pprint import pprint
     
 class cve:
+    results = {}
     path = "https://services.nvd.nist.gov/rest/json/cves/2.0?"
     versions: dict
-    results: {}
+    header ={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " \
+                                "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
 
     def __init__(self, versions: dict):
         assert versions is not None, "Version not exists"
         self.versions = versions
 
     def search_cve(self) :
-        i = 0
+        """ Research CVE given a group of services or a CPE from the Scan Result. 
+        Return  a dictionary with Version, CVE-id, CVE description and resources where the user can read  """
+        key_counter= 0
         results = {}
         if "cpe" in  self.versions:
-            params = {"CPE": self.versions["CPE"] + "isVulnerable"}
-            response_cpe = requests.get(self.path, params=params)
-            results = cve.get_vulnerabilities(response_cpe.json(), self.versions["CPE"])
-        for port, info in versions['ports'].items():
+            response_cpe = requests.get(self.path, params={"CPE": self.versions["CPE"] + "isVulnerable"}, headers=self.header)
+            key_counter +=1
+            results[key_counter] = cve.get_vulnerabilities(response_cpe.json(), self.versions["CPE"])
+        for port, info in self.versions['ports'].items():
             if 'version' in info:
-                params = {"keywordSearch":  info["version"]}
-                response_versione = requests.get(self.path, params = params)
-                if response_versione.status_code == 200:
-                    data = response_versione.json()
-                    vulnerabilities = cve.get_vulnerabilities(data, info["version"])
-                    i += 1
-                    results[i] = [vulnerabilities]
+                response_version = requests.get(self.path, params = {"keywordSearch":  info["version"]}, headers=self.header)
+                if response_version.status_code == 200:
+                    data = response_version.json()
+                    key_counter += 1
+                    results[key_counter] = [cve.get_vulnerabilities(data, info["version"])]
+
+        self.results = results
         return results
 
     @classmethod
     def get_vulnerabilities(cls, data: dict, version) -> dict:
+        """ Create a dictionary with Version, CVE-id, CVE Description e References"""
         results = {"Version": version}
         for cve in data["vulnerabilities"]:
                 results["id"] = cve["cve"]["id"]
@@ -39,7 +44,7 @@ class cve:
 
 
 
-versions = {
+"""versions = {
     'ports': {
         '21/tcp': {'service': 'ftp', 'version': 'vsftpd 2.3.4', 'state': 'open'},
         '22/tcp': {'service': 'ssh', 'version': 'OpenSSH 4.7p1 Debian 8ubuntu1', 'state': 'open'},
@@ -58,4 +63,4 @@ versions = {
     'os': {'name': 'Linux 2.6.9 - 2.6.33', 'accuracy': '100'}
 }
 cve_element = cve(versions)
-pprint(cve_element.search_cve())
+pprint(cve_element.search_cve())"""
