f4231ba541ba731067225627c1c6d96d961a7d9e
kali-seph <kali-seph@seph-infinito.homenet.telecomitalia.it>
2023-01-06 01:35:02 +0100

    + added a debugger mode for startscan, added a new advanced, and a welcome message with all button and gui each button

--- source_code/view/main_gui.py
+++ source_code/view/main_gui.py
@@ -33,7 +33,6 @@ class App(customtkinter.CTk):
         ip_var = customtkinter.StringVar(value="IP address")
         port_var = customtkinter.StringVar(value="Port")
         tcp_udp_var = customtkinter.StringVar(value="TCP-UDP")
-        type_adv_var = customtkinter.StringVar(value="Advanced")
         scan_type_var = customtkinter.StringVar(value="Shallow")
         scan_aggro_var = customtkinter.StringVar(value="0")
         chechbox_welcome_var = customtkinter.StringVar(value="on")
@@ -81,7 +80,7 @@ class App(customtkinter.CTk):
 
         def speed_test_button():
             speed_test_window = customtkinter.CTkToplevel()
-            speed_test_window.geometry(f"800x400")
+            speed_test_window.geometry(f"600x400")
             speed_test_window.title("Speed Test")
             speed_test_window.columnconfigure(0, weight=1)
             speed_test_window.rowconfigure(0, weight=1)
@@ -171,6 +170,67 @@ class App(customtkinter.CTk):
             chechbox_welcome = customtkinter.CTkCheckBox(master=frame_welcome, text="Apri al prossimo avvio", variable=chechbox_welcome_var, onvalue="on", offvalue="off")
             chechbox_welcome.grid(row=4, sticky="sw", pady=50)
 
+        def open_top_adv():
+            # started window and frame
+            adv_window = customtkinter.CTkToplevel()
+            adv_window.geometry(f"500x300")
+            adv_window.title("Advanced Options")
+
+            adv_frame = customtkinter.CTkFrame(master=adv_window, fg_color="transparent")
+            adv_frame.grid(row=0, pady=30, padx=30, sticky="nsew")
+            adv_frame.place(relx=0.5, rely=0.5, anchor="c")
+            
+            
+            opt1_var = customtkinter.StringVar()
+            opt2_var = customtkinter.StringVar()
+            opt3_var = customtkinter.StringVar()
+
+            # all the options in examples for debugging purposes
+            option1_check = customtkinter.CTkCheckBox(master=adv_frame, text="Example 1", variable=opt1_var, onvalue="Example 1")
+            option1_check.grid(row=0, column=0, sticky="n", pady=10)
+
+            option2_check = customtkinter.CTkCheckBox(master=adv_frame, text="Example 2", variable=opt2_var, onvalue="Example 2")
+            option2_check.grid(row=1, column=0, sticky="n", pady=10)
+
+            option3_check = customtkinter.CTkCheckBox(master=adv_frame, text="Example 3", variable=opt3_var, onvalue="Example 3")
+            option3_check.grid(row=2, column=0, sticky="n", pady=10)
+
+            # button to kill the window and store the variables
+            kill_button = customtkinter.CTkButton(master=adv_frame, text="OK", command=lambda: kill_window(adv_window))
+            kill_button.grid(row=3, sticky="se", padx=30, pady=30)
+            
+            def kill_window(top):
+                # storing variables and printing for debugging
+                global option_var_1 
+                global option_var_2
+                global option_var_3
+                option_var_1 = opt1_var.get()
+                option_var_2 = opt2_var.get()
+                option_var_3 = opt3_var.get()
+
+                print(option_var_1)
+                print(option_var_2)
+                print(option_var_3)
+
+                top.destroy()
+
+        # a debugging function in terminal
+        def start_scan():
+            print("Starting scan...")
+
+            ip = ip_var.get()
+            port = port_var.get()
+            tcp_udp = tcp_udp_var.get()
+            scan_type = scan_type_var.get()
+            scan_aggro = scan_aggro_var.get()
+
+            print("Ip: {}".format(ip))
+            print("Port: {}".format(port))
+            print("TCP/UDP: {}".format(tcp_udp))
+            print("Type: {}".format(scan_type))
+            print("Aggro: {}".format(scan_aggro))
+            print("Advanced: {} {} {}".format(option_var_1, option_var_2, option_var_3))
+
 
         # main frame with options and label
         self.main_frame = customtkinter.CTkFrame(self, fg_color="transparent")
@@ -202,8 +262,8 @@ class App(customtkinter.CTk):
         self.tcp_udp_option = customtkinter.CTkOptionMenu(master=self.center_frame, values=["TCP", "UDP"], variable=tcp_udp_var, width=100)
         self.tcp_udp_option.grid(row=0, column=2, sticky="w", padx=10)
 
-        # TODO: change advanced settings to top level with all check box
-        self.type_adv_option = customtkinter.CTkOptionMenu(master=self.center_frame, values=["Base", "Advanced"], variable=type_adv_var, width=100)
+        # advanced settings to top level with all check box
+        self.type_adv_option = customtkinter.CTkButton(master=self.center_frame, text="Advanced", command=open_top_adv, width=100)
         self.type_adv_option.grid(row=0, column=3, sticky="w", padx=10)
 
         # other options menu
@@ -214,7 +274,7 @@ class App(customtkinter.CTk):
         self.scan_aggro_option.grid(row=0, column=5, sticky="w", padx=10)
 
         # button scan
-        self.scan_button = customtkinter.CTkButton(self.center_frame, text="Scan", width=70, height=25)
+        self.scan_button = customtkinter.CTkButton(self.center_frame, text="Scan", width=70, height=25, command=start_scan)
         self.scan_button.grid(row=0, column=6, sticky="nsew", padx=10)
         
         # TODO: create a custom treeview for customtk
