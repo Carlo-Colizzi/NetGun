667b62762c07dc8a623fe90dff9420b457ab2f48
ducky <carlo.colizzi@gmail.com>
2023-01-08 01:04:01 +0100

    + added classes: tip and Storage_manager

---
+++ source_code/business_logic/tips/tip.py
@@ -0,0 +1,37 @@
+import xml.etree.ElementTree as ET
+from source_code.business_logic.testing_misconfigurations.misconfiguration import Misconfiguration
+import pprint
+class Tip:
+
+    def __int__(self):
+        pass
+
+    def print_toString(self):
+        print("service: ",self.service)
+        print("acronym: ", self.acronym)
+        print("description: ",self.description)
+        print("default_port: ",self.default_port)
+
+    @classmethod
+    def parse_tips_from_file(cls, path):
+        """Parse the misconfigurations contained in the specified file, returning a dictionary"""
+        tree = ET.parse(path)
+        root = tree.getroot()
+
+        service_dict = {}
+        for service in root:
+            t = Tip()
+            for child in service:
+                setattr(t, child.tag, child.text)
+            t.service = service.tag
+            service_dict[service.tag] = t
+
+        return service_dict
+
+if __name__ == "__main__":
+    path = "/home/ducky/Desktop/tip.xml"
+    tipsDict = Tip.parse_tips_from_file(path)
+    pprint.pprint(tipsDict)
+    for i in tipsDict:
+        tipsDict[i].print_toString()
+        print("\n")
---
+++ source_code/persistent/Storage_manager.py
@@ -0,0 +1,44 @@
+import os, pickle
+import pprint
+from source_code.business_logic.testing_misconfigurations.services_misconfigurations import Services_misconfigurations
+from source_code.business_logic.tips.tip import Tip
+
+class Storage_manager:
+    MISCONFIGURATIONS_XML_PATH = "storage/misconfiguration.xml"
+    MISCONFIGURATIONS_PKL_PATH = "storage/misconfigurations.pkl"
+
+    TIPS_XML_PATH = "storage/tip.xml"
+    TIPS_PKL_PATH = "storage//tip.pkl"
+
+    @classmethod
+    def load_resource(cls, xml_path, pkl_path, create_object_from_file):
+        if os.path.exists(pkl_path):
+            return Storage_manager.deserialize_object_from_file(pkl_path)
+        else:
+            obj = create_object_from_file(xml_path)
+            Storage_manager.serialize_object_into_file(obj,pkl_path)
+            return obj
+    @classmethod
+    def deserialize_object_from_file(cls,file):
+        obj = None
+        with open(file, 'rb') as f:
+            # Deserialize the object
+            obj = pickle.load(f)
+        return obj
+
+    @classmethod
+    def serialize_object_into_file(cls,obj,file):
+        with open(file, 'wb') as f:
+            # Serialize the object
+            pickle.dump(obj,f)
+
+    @classmethod
+    def load_misconfigurations(cls):
+        return Storage_manager.load_resource(Storage_manager.MISCONFIGURATIONS_XML_PATH, Storage_manager.MISCONFIGURATIONS_PKL_PATH, Services_misconfigurations.inizialize_object_from_file_xml)
+
+    @classmethod
+    def load_tips(cls):
+        return Storage_manager.load_resource(Storage_manager.TIPS_XML_PATH, Storage_manager.TIPS_PKL_PATH, Tip.parse_tips_from_file)
+
+if __name__ == "__main__":
+    pprint.pprint(Storage_manager.load_tips()["ftp"].print_toString())
