07e44f8bf333b789b8c0920c6e188ee459e31192
ShackWove <giulio.incoronato01@gmail.com>
2023-02-15 18:12:59 -0500

    + modified test files

---
+++ source_code/test/test_checkipandport.py
@@ -0,0 +1,3 @@
+import pytest
+class test_checkipandport:
+
--- source_code/test/test_create_report.py
+++ source_code/test/test_create_report.py
@@ -15,3 +15,27 @@ class Test_create_report:
         report = Report(path)
         report.create_report = make_tesable(report.create_report())
         assert report == True
+
+        """"
+         'ports': {'21/tcp': {'service': 'ftp',
+                              'state': 'open',
+                              'version': 'vsftpd 2.3.4'},
+                   '22/tcp': {'service': 'ssh',
+                              'state': 'open',
+                              'version': 'OpenSSH 4.7p1 Debian 8ubuntu1'},
+                   '23/tcp': {'service': 'telnet',
+                              'state': 'open',
+                              'version': 'Linux telnetd '},
+                   '25/tcp': {'service': 'smtp',
+                              'state': 'open',
+                              'version': 'Postfix smtpd '},
+                   '53/tcp': {'service': 'domain',
+                              'state': 'open',
+                              'version': 'ISC BIND 9.4.2'},
+                   '80/tcp': {'service': 'http',
+                              'state': 'open',
+                              'version': 'Apache httpd 2.2.8'}},
+         'status': 'up'},
+         os': {'accuracy': '100', 'name': 'Linux 2.6.9 - 2.6.33'},
+         {'Vulnerabilities': {'service': {'OpenSSH 4.7p1 Debian 8ubuntu1': []}}
+         '"""
