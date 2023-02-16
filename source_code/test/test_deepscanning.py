2d611ff3e58ac26c86f1ce411e70c435679a15e1
ShackWove <giulio.incoronato01@gmail.com>
2023-02-15 19:35:08 -0500

    + modified test files v3

--- source_code/test/test_deepscanning.py
+++ source_code/test/test_deepscanning.py
@@ -1,15 +1,22 @@
 import pytest
+
+from source_code.business_logic.scanner.filter import Filter
 from source_code.business_logic.scanner.scan import Scan
+from source_code.business_logic.scanner.target import Target
 
 
 class Test_deep_scanning:
 
     @pytest.mark.TC1_2_01
     def test_if_no_input(self):
-        scan = Scan("192.168.1.198", "1-1024", "")
-        assert scan.start_scan() is  None
+        target = Target("192.168.1.198", "1-1024")
+        filter = Filter()
+        scan = Scan(target, filter)
+        assert scan.scan_mode == "SHALLOW"
 
     @pytest.mark.TC1_2_02
     def test_if_input_deep(self):
-        scan = Scan("192.168.1.198", "1-1024", "DEEP")
-        assert scan.start_scan() is not None
+        target = Target("192.168.1.198", "1-1024")
+        filter = Filter()
+        scan = Scan(target, filter,"DEEP")
+        assert scan.scan_mode == "DEEP"
