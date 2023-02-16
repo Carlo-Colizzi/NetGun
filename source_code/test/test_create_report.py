260a4b9549afcc8dc70ccddb53545aacf4a7fb3f
ducky <carlo.colizzi@gmail.com>
2023-02-16 01:23:59 +0100

    + Added test_reate_report final test

--- source_code/test/test_create_report.py
+++ source_code/test/test_create_report.py
@@ -4,19 +4,22 @@ from source_code.business_logic.report.report import Report
 
 class Test_create_report:
 
+    @pytest.mark.TC1_3_01
     def test_max_lenght_path(self):
         path = "/home/pc_user/filenamedwdwfdwehfuoefhowhfsdhiuhviuehviushviuewqhfviuhewqipuhgbeuhwqvrbuoewqgbfuoewqyrcfbuhogqygbyuhgbqyfhgeyugcbqeuwydrgwycgvuwycevbquyhwgvbwuhdgvcuhdgvcquhdwvcuhdvbcuohwqvedbcuhqdvbcuqwvbcuhdqcbihqedrcbiqprjebnvciedskjcpkjamcnpkjndmcknsadcoknajdcnkjsdnc.txt"
         assert Report.check_path(path) == False
 
-
+    @pytest.mark.TC1_3_02
     def test_standard_path(self):
         path = "/home/users/Documents/filename.txt"
         assert Report.check_path(path) == True
 
+    @pytest.mark.TC1_3_03
     def test_multiple_slash(self):
         path = "/home/users///Documents///filenamedwdwfdwehfuoefhowhfsdhiuhviuehviushviuewqhfviuhewqipuhgbeuhwqvrbuoewqgbfuoewqyrcfbuhogqygbyuhgbqyfhgeyugcbqeuwydrgwycgvuwycevbquyhwgvbwuhdgvcuhdgvcquhdwvcuhdvbcuohwqvedbcuhqdvbcuqwvbcuhdqcbihqedrcbiqprjebnvciedskjcpkjamcnpkjndmcknsadcoknajdcnkjsdnc.txt"
         assert Report.check_path(path) == False
 
+    @pytest.mark.TC1_3_04
     def test_inverted_slash(self):
         path =  "//home\pcexp//filereport.txt"
         assert Report.check_path(path) == False
