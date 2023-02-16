7c27df2591f871e04a95e39507c4757bc2f26630
ShackWove <giulio.incoronato01@gmail.com>
2023-02-15 19:15:47 -0500

    + modified test files

--- source_code/test/test_deepscanning.py
+++ source_code/test/test_deepscanning.py
@@ -4,12 +4,12 @@ from source_code.business_logic.scanner.scan import Scan
 
 class Deep_scanning:
 
+    @pytest.mark.nullo
     def test_if_no_input(self):
         scan = Scan("192.168.1.198", "1-1024", "")
-        assert scan.start_scan() != None
+        assert scan.start_scan() is None
 
+    @pytest.mark.deep
     def test_if_input_deep(self):
         scan = Scan("192.168.1.198", "1-1024", "DEEP")
-        assert scan.start_scan() != None
-
-        
+        assert scan.start_scan() is None
