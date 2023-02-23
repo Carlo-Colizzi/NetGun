2652a387f2ea4855ce939bb3686547fb7eab9351
ShackWove <giulio.incoronato01@gmail.com>
2023-02-23 07:17:32 -0500

    + added file for deb package

--- setup.py
+++ setup.py
@@ -8,13 +8,12 @@ with open('README.md') as fh:
     long_description = fh.read()
 
 setup(name='netgun',
-      version='0.1.1-beta',
+      version='0.1',
       description='NetGun, powerful penetration tool',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='https://github.com/MyCr4ck/NetGun_Classe03',
       entry_points={'console_scripts': ['netgun = source_code.view.main_gui:__init__']},
-      data_files=[('share/applications/', ['netgun.desktop'])],
       packages=find_packages(),
       install_requires=open('requirements.txt').readlines(),
       keywords="netgun pentest tool",
@@ -22,5 +21,4 @@ setup(name='netgun',
       author='NetGun_Classe03resto2',
       author_email='',
       license='GPL-3.0',
-      zip_safe=False
 )
